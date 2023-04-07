1dff3977ba901ee2de5f1576cca2d35f6409fd33
Savy <savy.novachez011@gmail.com>
2023-04-07 22:37:21 +0530

    Finished Ex 2.6

---
+++ Part_2/phonebook/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const App = () => {
+	const [ persons, setPersons ] = useState([
+		{ 
+			id: 1,
+			name: 'Savy'
+		}
+	])
+	const [ newPerson, setNewPerson] = useState('')
+	
+	const addPerson = (event) => {
+		event.preventDefault()
+		const personObject = {
+			id: persons.length + 1,
+			name: newPerson
+		}
+
+		setPersons(persons.concat(personObject))
+		setNewPerson('')
+	}
+
+
+	const handleChange = (event) => {
+		setNewPerson(event.target.value)
+	}
+	
+	
+	return (
+		<div>
+			<h2>PhoneBook</h2>
+			<form onSubmit={addPerson}>
+				<div>
+					Name: <input value={newPerson} onChange={handleChange}/>
+				</div>
+				<div>
+					<button type='submit'>Add</button>
+				</div>
+			</form>
+			
+			<h2>Contacts</h2>
+			{persons.map(item => 
+				<p key={item.id}>{item.name}</p>
+			)}	
+		</div>
+	)
+}
+
+export default App;
