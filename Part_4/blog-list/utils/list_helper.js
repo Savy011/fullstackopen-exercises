65e60d953b7e0231735bf4c3d43b0bb75ed56b8f
Savy <savy.novachez011@gmail.com>
2023-04-26 14:37:12 +0530

    Finished Ex 4.4

--- Part_4/blog-list/utils/list_helper.js
+++ Part_4/blog-list/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
 	return 1
 }
 
+const totalLikes = (blogs) => {
+	let sum = 0
+	const total = blogs.forEach(blog => sum += blog.likes)
+
+	return blogs.length !== 0 ? sum : 0
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 }
