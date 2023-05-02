85d86c45e75421d8d3da7c021a50a0e8ad8e6973
Savy <savy.novachez011@gmail.com>
2023-05-02 22:24:33 +0530

    Finished Ex 4.19

--- Part_4/blog-list/controllers/blogs.js
+++ Part_4/blog-list/controllers/blogs.js
@@ -1,6 +1,18 @@
+const { JWT_SECRET } = require('../utils/config')
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const tokenExtracter = request => {
+	const authorization = request.get('authorization')
+
+	if (authorization && authorization.startsWith('Bearer ')) {
+		return authorization.replace('Bearer ','')	
+	}
+
+	return null
+}
 
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -9,7 +21,14 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-	const user = await User.findOne({})
+	const decodedToken = jwt.verify(tokenExtracter(request), JWT_SECRET)
+	if (!decodedToken.id) {
+		return response.status(401).json({
+			error: 'Token Invalid'
+		})
+	}
+
+	const user = await User.findById(decodedToken.id)
 
 	const blog = new Blog({
 		...request.body,
