33daa29d03f1cc43e85e938c48626d99be2f4937
Savy <savy.novachez011@gmail.com>
2023-05-02 22:06:48 +0530

    Finished Ex 4.18

--- Part_4/blog-list/utils/config.js
+++ Part_4/blog-list/utils/config.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 const MONGODB_URL = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URL : process.env.MONGODB_URL
+const JWT_SECRET = process.env.SECRET
 
 module.exports = {
 	MONGODB_URL,
-	PORT
+	PORT,
+	JWT_SECRET
 }
