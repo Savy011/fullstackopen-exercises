d047fe9dac3ba9eee611d61f1dcf33d62ec44fc8
Savy <savy.novachez011@gmail.com>
2023-05-05 14:56:40 +0530

    Finished Ex 5.7

--- Part_5/bloglist-frontend/src/components/Blog.js
+++ Part_5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,24 @@
+import { useState } from "react"
+
 const Blog = ({ blog }) => {
+	const [ blogObj, setBlogObj ] = useState(blog)
+	const [ visible, setVisible ] = useState(false)
+	
+	const showWhenVisible = { display: visible ? '' : 'none' }
+	const buttonLabel = visible ? 'Hide' : 'Show'
+		
 	return (
-		<div>
-    		<p><i>{blog.title}</i> - {blog.author}</p>
+		<div className="blog" >
+    		<div>
+				<i>{blogObj.title}</i> - {blogObj.author}
+				&nbsp;
+				<button onClick={() => setVisible(!visible)} >{buttonLabel}</button>
+			</div>
+			<div style={showWhenVisible} >
+				<p><b>Url:</b> {blogObj.url}</p>
+				<p><b>Likes:</b> {blogObj.likes} <button> 👍 </button> </p>
+				<p><b>User:</b> {blogObj.user.name}</p>
+			</div>
 		</div>
 	)
 }
