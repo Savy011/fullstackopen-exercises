2b091a3048ef59b1db7e622b40b994932e50b059
Savy <savy.novachez011@gmail.com>
2023-05-05 17:13:35 +0530

    Finished Ex 5.10

--- Part_5/bloglist-frontend/src/App.js
+++ Part_5/bloglist-frontend/src/App.js
@@ -132,7 +132,7 @@ const App = () => {
 
 			<h3>Blog List</h3>
 			<div>
-    			{blogs.map(blog => <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />)}
+    			{blogs.sort((a, b) => b.likes - a.likes).map(blog => <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />)}
 			</div>
 
 		</div>
