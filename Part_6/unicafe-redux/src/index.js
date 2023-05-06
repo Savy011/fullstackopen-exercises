8be20bbb9ee17264b9cd9f5dfb7850430d962f7c
Savy <savy.novachez011@gmail.com>
2023-05-06 23:02:28 +0530

    Finished Ex 6.2

--- Part_6/unicafe-redux/src/index.js
+++ Part_6/unicafe-redux/src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { createStore } from 'redux'
+import counterReducer from './reducers/counterReducer'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(counterReducer)
+
+const App = () => {
+	const good = () => {
+		store.dispatch({ type: 'GOOD' })
+	}
+	const okay = () => {
+		store.dispatch({ type: 'OK' })
+	}
+	const bad = () => {
+		store.dispatch({ type: 'BAD' })
+	}
+	const reset = () => {
+		store.dispatch({ type: 'ZERO' })
+	}
+
+	return (
+		<div>
+			<button onClick={good}>Good</button>&nbsp;
+			<button onClick={okay}>Okay</button>&nbsp;
+			<button onClick={bad}>Bad</button>&nbsp;
+			<button onClick={reset}>Reset</button>&nbsp;
+			
+			<div>Good: {store.getState().good}</div>
+			<div>Okay: {store.getState().ok}</div>
+			<div>Bad: {store.getState().bad}</div>
+		</div>
+	)
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+	root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
