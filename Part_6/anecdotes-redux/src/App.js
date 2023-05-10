4ecd77b08c787971ae078d1848b4b313fb1bfea3
Savy <savy.novachez011@gmail.com>
2023-05-10 11:59:08 +0530

    Finished Ex 6.7

--- Part_6/anecdotes-redux/src/App.js
+++ Part_6/anecdotes-redux/src/App.js
@@ -1,16 +1,10 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { increaseVoteOf, createAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
     const anecdotes = useSelector(state => state)
     const dispatch = useDispatch()
-
-    const addAnecdote = event => {
-        event.preventDefault()
-        const content = event.target.anecdote.value
-        event.target.anecdote.value = ''
-        dispatch(createAnecdote(content))
-    }
 
     const vote = (id) => {
         dispatch(increaseVoteOf(id))
@@ -18,27 +12,20 @@ const App = () => {
 
     return (
         <div>
-        <h2>Anecdotes</h2>
-        {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
-            <div key={anecdote.id}>
-            <div>
-                <p>{anecdote.content}</p>
-            </div>
-            <div>
-                Has {anecdote.votes} Votes
-                &nbsp;
-                <button onClick={() => vote(anecdote.id)}>Vote</button>
-            </div>
-            </div>
-        )}
-        <h2>Create New</h2>
-        <form onSubmit={addAnecdote}>
-            <p>
-                <input name='anecdote' />
-                &nbsp;
-                <button type='submit'>Create</button>
-            </p>
-        </form>
+            <h2>Anecdotes</h2>
+            {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+                <div key={anecdote.id}>
+                <div>
+                    <p>{anecdote.content}</p>
+                </div>
+                <div>
+                    Has {anecdote.votes} Votes
+                    &nbsp;
+                    <button onClick={() => vote(anecdote.id)}>Vote</button>
+                </div>
+                </div>
+            )}
+            <AnecdoteForm />
         </div>
     )
 }
