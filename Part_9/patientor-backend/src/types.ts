99536963ddac8d169ae6ac3d6a767486fb0f6bc2
Savy011 <106910589+Savy011@users.noreply.github.com>
2023-05-29 09:10:07 +0530

    Finished Ex 9.22

--- Part_9/patientor-backend/src/types.ts
+++ Part_9/patientor-backend/src/types.ts
@@ -10,8 +10,48 @@ export enum Gender {
 	Other = 'other'
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
+export enum HealthCheckRating {
+	"Healthy" = 0,
+	"LowRisk" = 1,
+	"HighRisk" = 2,
+	"CriticalRisk" = 3
+}
+
+interface Discharge {
+	date: string;
+	criteria: string;
+}
+
+interface SickLeave {
+	startDate: string;
+	endDate: string;
+}
+
+interface BaseEntry {
+	id: string;
+	description: string;
+	date: string;
+	specialist: string;
+	diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+	type: "HealthCheck";
+	healthCheckRating: HealthCheckRating;
+}
+
+export interface HospitalEntry extends BaseEntry {
+	type: "Hospital";
+	discharge: Discharge;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+	type: "OccupationalHealthcare";
+	employerName: string;
+	sickLeave?: SickLeave;
+}
+
+export type Entry = HospitalEntry | HealthCheckEntry | OccupationalHealthcareEntry
 
 export interface Patient {
 	id: string;
